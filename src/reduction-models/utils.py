fdbe6986aca17b956edd894304873071071ce669
André Marques <andre.costa.marques@hotmail.com>
2025-12-22 21:00:31 -0300

    stashing changes

--- src/reduction-models/utils.py
+++ src/reduction-models/utils.py
@@ -1,1 +1,5 @@
 # codigo para colocar todas as funções de utilidades para os testes de reduction-models
+# funções que precisamos fazer:
+# wrapper pra retornar só o string byte
+# wrapper pra retornar a imagem mascarada
+
